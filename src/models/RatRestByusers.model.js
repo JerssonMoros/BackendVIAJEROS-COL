953e53559ba462eaa12067aba44e6cc5dc883e23
Fabianuseche <115897847+Fabianuseche@users.noreply.github.com>
2024-05-20 20:51:26 -0500

    modelo_calificacion

---
+++ src/models/RatRestByusers.model.js
@@ -0,0 +1,41 @@
+// const { Schema, model } = require('mongoose');
+
+// const userSchema = Schema({
+//     //Defino mis atrubutos del esquema.
+// })
+
+// module.exports = model( 'Users', userSchema )
+
+const { Schema, model } = require('mongoose');
+
+const ratingSchema = new Schema({
+  
+    quality: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    customerService: {
+        type: String,
+        required: true,
+    },
+    comments: {
+        type: String,
+        required: true
+    },
+    restaurantID: {
+        type: Schema.Types.ObjectId,
+        ref: 'Restaurante',
+        required: true
+    },
+    userID: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+module.exports = model('Rating', ratingSchema);
