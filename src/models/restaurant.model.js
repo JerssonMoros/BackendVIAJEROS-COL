bb1ae7f2b05c37f02a4fdf0a43ebafcdb153c7c4
9anavaleria <ana98cip@gmail.com>
2024-05-20 20:36:17 -0500

    creación modelo restaurante

---
+++ src/models/restaurant.model.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const restauranteSchema = Schema({
+    //Defino mis atrubutos del esquema.
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true, //Elimina los espacios al principio y al final del correo
+        lowercase: true, //El correo lo pasa a minusculas
+        unique: [true, 'El correo ya existe'] //Validar que el correo no exista
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+module.exports = model( 'Users', userSchema )
