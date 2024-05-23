3d4bc660fe1bbfc31d89a2f2affff6dad9d4d85b
9anavaleria <95309779+9anavaleria@users.noreply.github.com>
2024-05-23 16:56:31 -0500

    ADICION

--- src/controllers/restaurante.controller.js
+++ src/controllers/restaurante.controller.js
@@ -1,54 +1,108 @@
-const Restaurant = require('../models/restaurant.model.js')
+const Restaurant = require("../models/restaurant.model.js");
 
+const verRestaurante = async (req, res) => {
+    const users = await Restaurant.find().populate('nombre_restaurante', 'ubicacion', 'categoria', 'descripcion', 'fotografias', 'horario','id_usuario');
+    return res.json({
+        ok: "ok",
+        data: restaurante,
+    });
+};
 const ingresarRestaurante = (req, res) => {
     try {
-        const { nombre_restaurante, ubicacion, categoria, descripcion, fotografias, horario, id_usuario} = req.body;
-        const data = { nombre_restaurante, ubicacion, categoria, descripcion, fotografias, horario, id_usuario}
+        const {nombre_restaurante, ubicacion, categoria, descripcion, fotografias, horario,id_usuario,} = req.body;
+        const data = {nombre_restaurante, ubicacion, categoria, descripcion, fotografias, horario, id_usuario,};
         const restaurante = new Restaurant(data);
         restaurante.save();
         return res.status(201).json({
             ok: true,
-            msg: 'Creacion con exito.',
-            restaurante
-        })
+            msg: "Creacion con exito.",
+            restaurante,
+        });
     } catch (error) {
         console.log(error);
         if (error.code === 11000) {
             // El código 11000 es el código de error de duplicado en MongoDB
-            res.status(400).send({ error: '' });
+            res.status(400).send({ error: "" });
         } else {
             // Manejar otros posibles errores
-            res.status(500).send({ error: '' });
+            res.status(500).send({ error: "" });
         }
     }
-
-}
+};
 
 const actualizarRestaurante = (req, res) => {
-    const { id_restaurante } = req.params;
-    const { newNombre_restaurante, newUbicacion, newCategoria, newDescripcion, newFotografias, newHorario, newId_usuario, newMenus, newCalificacion } = req.body;
-    const restaurantId = parseInt(id_restaurante);
-    const restaurant = restaurantes.find(restaurantes => restaurant.id_restaurante === restaurantId);
-    if (restaurant) {
-        restaurantes.nombre_restaurante = newNombre_restaurante;
-        restaurantes.ubicacion = newUbicacion;
-        restaurantes.categoria = newCategoria;
-        restaurantes.descripcion = newDescripcion;
-        restaurantes.fotografias = newFotografias;
-        restaurantes.horario = newHorario;
-        restaurantes.id_usuario = newId_usuario;
-        restaurantes.menus = newMenus;
-        restaurantes.calificacion = newCalificacion;
-        res.json({ message: `Restaurante ${id_restaurante} actualizado` });
-    } else {
-        res.status(404).json({ message: `Restaurante ${id_restaurante} no encontrado` });
+    try {
+        const {id} = req.params;
+        const {newNombre_restaurante,
+            newUbicacion,
+            newCategoria,
+            newDescripcion,
+            newFotografias,
+            newHorario,
+            newId_usuario,
+            newMenus,
+            newCalificacion,
+        } = req.body;
+        const restaurantId = parseInt(id);
+        const restaurant = restaurantes.find(
+            (restaurantes) => restaurant.id === restaurantId
+        );
+        if (restaurant) {
+            restaurantes.nombre_restaurante = newNombre_restaurante;
+            restaurantes.ubicacion = newUbicacion;
+            restaurantes.categoria = newCategoria;
+            restaurantes.descripcion = newDescripcion;
+            restaurantes.fotografias = newFotografias;
+            restaurantes.horario = newHorario;
+            restaurantes.id_usuario = newId_usuario;
+            restaurantes.menus = newMenus;
+            restaurantes.calificacion = newCalificacion;
+            res.json({ message: `Restaurante ${id_restaurante} actualizado` });
+        } else {
+            res
+                .status(404)
+                .json({ message: `Restaurante ${id_restaurante} no encontrado` });
+        }
+        console.log(restaurantes);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            errors: ["Error en el servidor"],
+        });
     }
-    console.log(restaurantes);
+};
+const borrarRestaurante = async( req, res) => {
+    try {
+        const {id} = req.params;
+        let restaurante = await Restaurant.findById(id);
 
+        if(!restaurante){
+            return res.status(400).json({
+                ok: `No existe un restaurante con id ${id}`,
+                ok: false 
+            })
+        }
 
+        restaurante = await Restaurant.findByIdAndUpdate(id, {state: false});
+
+        return res.status(400).json({
+            ok: 'User deleted',
+            ok: true,
+        })
+
+    }catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            errors: ['Error en el servidor']
+        })
+    }
 }
 
 module.exports = {
+    verRestaurante,
     ingresarRestaurante,
-    actualizarRestaurante
-}
+    actualizarRestaurante,
+    borrarRestaurante
+};
